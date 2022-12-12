c65e00b544985a2a10a73ab12571cee6b6d6da37
Viktor <vjagebrant@gmail.com>
2022-12-12 20:19:15 +0100

    adapter create

--- app/src/main/java/com/example/mapapp/ListActivity.kt
+++ app/src/main/java/com/example/mapapp/ListActivity.kt
@@ -2,6 +2,8 @@ package com.example.mapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListActivity : AppCompatActivity() {
 
@@ -11,6 +13,9 @@ class ListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list)
 
         var placeList = initialisePlaceList()
+
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
     }
 
---
+++ app/src/main/java/com/example/mapapp/PlaceRecycleAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.mapapp
+
+import android.content.Context
+
+class PlaceRecycleAdapter(val context: Context, val places : List<Place>) {
+    
+}
