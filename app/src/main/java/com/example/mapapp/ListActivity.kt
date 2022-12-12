a2c6f2b6aacb34c3c25c2fe93dc0f5353a6dc9a7
Viktor <vjagebrant@gmail.com>
2022-12-12 20:13:23 +0100

    small changes

--- app/src/main/java/com/example/mapapp/ListActivity.kt
+++ app/src/main/java/com/example/mapapp/ListActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ListActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        var placeList = initialisePlaceList()
+
     }
+
+    fun initialisePlaceList() : List<Place>{
+        return mutableListOf<Place>(Place("Gothenburg","Big",11.1,12.1,"Bob"),
+                                    Place("Malmö","Smaller",8.2,2.2,"Ron"),
+                                    Place("Lund","Cold",6.3,3.3,"Red"))
+    }
+
 }
