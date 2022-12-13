157ed9daf2d95aa8e9a1f2119f7ced6ded373923
Viktor <vjagebrant@gmail.com>
2022-12-13 16:25:31 +0100

    working upload

--- app/src/main/java/com/example/mapapp/AddPlaceActivity.kt
+++ app/src/main/java/com/example/mapapp/AddPlaceActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 
 class AddPlaceActivity : AppCompatActivity() {
@@ -27,14 +28,27 @@ class AddPlaceActivity : AppCompatActivity() {
 
         val addButton = findViewById<Button>(R.id.uploadPlaceButton)
         addButton.setOnClickListener {
-
             addPlace(placeNameEditText.text.toString(), descEditText.text.toString(),
                 10.1,10.1, authorEditText.text.toString())
         }
     }
 
     fun addPlace(place : String, desc : String, lat : Double, lng : Double, author : String){
+
+        if(place.isEmpty() || desc.isEmpty() || author.isEmpty()){
+            Toast.makeText(applicationContext,"Empty field",Toast.LENGTH_LONG).show()
+            return
+        }
+
         var item = Place(place, desc, lat, lng, author)
         db.collection("places").add(item)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Toast.makeText(applicationContext,"Place added!",Toast.LENGTH_LONG).show()
+                    finish()
+                } else{
+                    Toast.makeText(applicationContext,"Failed. ${task.exception}",Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
