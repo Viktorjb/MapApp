3596804aa70ad803e3f8478263ebce9c83ec3158
Viktor <vjagebrant@gmail.com>
2022-12-13 16:14:23 +0100

    etc

--- app/src/main/java/com/example/mapapp/AddPlaceActivity.kt
+++ app/src/main/java/com/example/mapapp/AddPlaceActivity.kt
@@ -2,10 +2,39 @@ package com.example.mapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AddPlaceActivity : AppCompatActivity() {
+
+    lateinit var db : FirebaseFirestore
+
+    lateinit var placeNameEditText : EditText
+    lateinit var descEditText : EditText
+    lateinit var authorEditText : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_place)
+
+        db = FirebaseFirestore.getInstance()
+
+        placeNameEditText = findViewById(R.id.placeNameEditText)
+        descEditText = findViewById(R.id.descEditText)
+        authorEditText = findViewById(R.id.authorEditText)
+
+        val addButton = findViewById<Button>(R.id.uploadPlaceButton)
+        addButton.setOnClickListener {
+
+            addPlace(placeNameEditText.text.toString(), descEditText.text.toString(),
+                10.1,10.1, authorEditText.text.toString())
+        }
+    }
+
+    fun addPlace(place : String, desc : String, lat : Double, lng : Double, author : String){
+        var item = Place(place, desc, lat, lng, author)
+        db.collection("places").add(item)
     }
 }
