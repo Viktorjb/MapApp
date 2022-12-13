85f9e701b3f54ef8cd2980e4a69d06c8b648ab6a
Viktor <vjagebrant@gmail.com>
2022-12-13 16:43:16 +0100

    working before fix

--- app/src/main/java/com/example/mapapp/ListActivity.kt
+++ app/src/main/java/com/example/mapapp/ListActivity.kt
@@ -6,13 +6,18 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ListActivity : AppCompatActivity() {
+
+    lateinit var db : FirebaseFirestore
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        db = FirebaseFirestore.getInstance()
 
         var placeList = initialisePlaceList()
 
@@ -34,6 +39,18 @@ class ListActivity : AppCompatActivity() {
     }
 
     fun initialisePlaceList() : List<Place>{
+
+        val docRef = db.collection("places")
+        var placeList = mutableListOf<Place>()
+
+        docRef.get().addOnSuccessListener { documentSnapShot ->
+            for(document in documentSnapShot.documents){
+                
+
+            }
+        }
+
+
         return mutableListOf<Place>(Place("Gothenburg","Big",11.1,12.1,"Bob"),
                                     Place("Malmö","Smaller",8.2,2.2,"Ron"),
                                     Place("Lund","Cold",6.3,3.3,"Red"))
