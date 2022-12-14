777e350ae496a3fc7400e0aa14df1d82dd365422
Viktor <vjagebrant@gmail.com>
2022-12-14 18:36:53 +0100

    place markers on map

--- app/src/main/java/com/example/mapapp/MapsActivity.kt
+++ app/src/main/java/com/example/mapapp/MapsActivity.kt
@@ -10,14 +10,20 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.mapapp.databinding.ActivityMapsBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    lateinit var db : FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        db = FirebaseFirestore.getInstance()
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -44,5 +50,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        placeMarkersFromFirebase()
+    }
+
+    fun placeMarkersFromFirebase(){
+        var placeList = mutableListOf<Place>()
+        val docRef = db.collection("places")
+        docRef.get().addOnSuccessListener { documentSnapShot ->
+            for(document in documentSnapShot.documents){
+                val plc = document.toObject<Place?>()
+                if(plc != null){
+                    placeList.add(plc)
+                }
+            }
+
+            for(plc : Place in placeList){
+                var position = LatLng(plc.lat!!,plc.lng!!)
+                var marker = mMap.addMarker(
+                    MarkerOptions().position(position).title(plc.name).snippet(plc.desc))
+            }
+
+        }
+
     }
 }
