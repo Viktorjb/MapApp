afe7234912c1e01b2e991f53541f5ad6b13983ae
Viktor <vjagebrant@gmail.com>
2022-12-14 16:18:12 +0100

    gps with permission

--- app/src/main/java/com/example/mapapp/AddPlaceActivity.kt
+++ app/src/main/java/com/example/mapapp/AddPlaceActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mapapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -20,6 +23,7 @@ class AddPlaceActivity : AppCompatActivity() {
     lateinit var locationProvider : FusedLocationProviderClient
     lateinit var locationRequest : LocationRequest
     lateinit var locationCallback : LocationCallback
+    private val REQUEST_LOCATION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -32,6 +36,7 @@ class AddPlaceActivity : AppCompatActivity() {
         descEditText = findViewById(R.id.descEditText)
         authorEditText = findViewById(R.id.authorEditText)
 
+        // GPS stuff
         locationProvider = LocationServices.getFusedLocationProviderClient(this)
         locationRequest = LocationRequest.Builder(5000).build()
         locationCallback = object : LocationCallback(){
@@ -41,6 +46,12 @@ class AddPlaceActivity : AppCompatActivity() {
                 }
             }
         }
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION)
+        }
+        // GPS stuff end
 
         val addButton = findViewById<Button>(R.id.uploadPlaceButton)
         addButton.setOnClickListener {
@@ -66,5 +77,43 @@ class AddPlaceActivity : AppCompatActivity() {
                     Toast.makeText(applicationContext,"Failed. ${task.exception}",Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    fun startLocationUpdates(){
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationProvider.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_LOCATION){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startLocationUpdates()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    fun stopLocationUpdates(){
+        locationProvider.removeLocationUpdates(locationCallback)
     }
 }
